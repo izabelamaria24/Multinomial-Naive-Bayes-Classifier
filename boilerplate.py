99c4ff44e49d2194033cbfb9cd72518315829a1a
raizojpg <diride4fun@gmail.com>
2024-11-09 23:57:17 +0200

    Added: Bayes Test Functions

--- boilerplate.py
+++ boilerplate.py
@@ -1,7 +1,10 @@
 import csv
 import re
+import random
 
-# MAX_COUNTER = 1950
+length_all_words_from_our_planet = 0
+
+MAX_COUNTER = 1000000
 DRAMA = "drama"
 HORROR = "horror"
 ROMANCE = "romance"
@@ -20,37 +23,100 @@ categories_dict = {
 }
 
 # container for ALL of the plots described below
-plots = []
+plots = {
+    DRAMA: [], 
+    HORROR: [],
+    ROMANCE: [],
+    ADVENTURE: [],
+    COMEDY: []
+}
 
-def split_train_test_dataset():
-    # TODO -> split the dataset into training and test set (80%, 20%)
+test_plots = {
+    DRAMA: [], 
+    HORROR: [],
+    ROMANCE: [],
+    ADVENTURE: [],
+    COMEDY: []
+}
 
 
 def preprocess_plot(plot):
     # TODO -> preprocess given plot
-    # return a list of words (tokens)
+    # return a list of words (tokens)'
+    tokens = set([word for word in pattern.findall(plot) if not word[0].isupper()])
+    return tokens
 
+def open_func():
+    with open('filtered_movies.csv', mode='r', encoding='utf-8') as file:
+        csv_reader = list(csv.reader(file))[1:]
+        #random.shuffle(csv_reader)
+        num_rows = int(len(csv_reader) * 0.8)
 
-with open('filtered_movies.csv', mode='r', encoding='utf-8') as file:
-    csv_reader = csv.reader(file)
+        # for every row (plot), preprocess it, and keep only the preprocessed words from the plot (tokens) 
+        # keep a list with all of the data (a list of tuples (preprocessed plot (string), genre (string)))
+        # we can use a dict too, but then we will need to filter based on the genre when we need it
 
-    # for every row (plot), preprocess it, and keep only the preprocessed words from the plot (tokens) 
-    # keep a list with all of the data (a list of tuples (preprocessed plot (string), genre (string)))
-    # we can use a dict too, but then we will need to filter based on the genre when we need it
+        for row in csv_reader[:num_rows]:
+            plot = row[0]
+            genre = row[1]
+            if len(plots[genre]) > MAX_COUNTER:
+                continue
+            tokens = preprocess_plot(plot)
+            plots[genre].append(tokens)
 
+        for row in csv_reader[num_rows:]:
+            plot = row[0]
+            genre = row[1]
+            tokens = preprocess_plot(plot)
+            test_plots[genre].append(tokens)
 
+total_plots = 0
 # for every category, iterate through its plots 
 # for each word, increment the counter 
 def category_tokens():
     # TODO -> populate the categories_dict
+    for category, category_plots in plots.items():
+        for plot in category_plots:
+            for word in plot:
+                categories_dict[category][word] = categories_dict[category].get(word, 0) + 1
 
 
-bayes = {} # dict for keeping probabilities
-def bayes_category(category):
-    # TODO -> calculate bayes for category
 
-# bayes -> for every category compute the probability to contain each word from its list
-def bayes():
-    # TODO -> iterate through the categories and compute bayes
-    # return the maximum value found
+bayes_category_dict = {} # dict for keeping probabilities
+def test_bayes(plot):
+    # TODO -> test the bayes on a given plot
+    # return the category with the highest probability
+    for category in categories_dict:
+        prod = len(plots[category]) / total_plots
+        for word in plot:
+            if categories_dict[category].get(word, 0) == 0:
+                continue
+            prod *= categories_dict[category][word]/len(plots[category])
+        bayes_category_dict[category] = prod
+    return max(bayes_category_dict, key=bayes_category_dict.get)
 
+def test():
+    total_test_plots = sum([len(test_plots[category]) for category in test_plots])
+    accuracy = 0
+    for category in test_plots:
+        for plot in test_plots[category]:
+            #print(f"{test_bayes(plot)}\t{category}")
+            if test_bayes(plot) == category:
+                accuracy += 1
+    return accuracy / total_test_plots
+
+
+def main():
+    open_func()
+    global total_plots
+    total_plots = sum([len(plots[category]) for category in plots])
+    length_all_words_from_our_planet = sum([len(plot) for plot in plots.values()])
+    category_tokens()
+
+    [print(len(x)) for x in plots.values()]
+    print()
+    [print(len(x)) for x in test_plots.values()]
+    print(test())
+
+if __name__ == "__main__":
+    main()
--- main.py
+++ main.py
@@ -10,10 +10,15 @@ categories_count = {"drama": 0, "comedy":0, "horror":0, "romance":0, "adventure"
 dict_all_words_from_our_planet = {}
 
 
+total_categories_count = 0
 total_words_count = 0
 plot_avg = 0
 i = 0
 
+#categories[category_name][
+#                            [words : [nr of sets in which it appears, prob?]] union of all sets
+#                            set(words in plots)  
+#                           ]
 
 with open('filtered_movies.csv', mode='r', encoding='utf-8') as file:
     csv_reader = csv.reader(file)
@@ -23,22 +28,26 @@ with open('filtered_movies.csv', mode='r', encoding='utf-8') as file:
             continue
         i += 1
         current_category_dict = categories[row[1]]
-        categories_count[row[1]]+=1
-        if categories_count[row[1]]>MAX_COUNTER:
+        if categories_count[row[1]]>MAX_COUNTER-1:
             continue
+        categories_count[row[1]]+=1
+        total_categories_count += 1
         words = pattern.findall(row[0])
+        plots = set(words)
+        print()
         plot_avg += len(words)
         for word in words:
             #w = word.strip('.,!?()[]\{\}-;:"/')
             if word[0] >= 'A' and word[0] <= 'Z':
                 continue
-            dict_all_words_from_our_planet[word] = [dict_all_words_from_our_planet.get(word, (0,0))[0] + 1, 0]
+            #dict_all_words_from_our_planet[word] = [dict_all_words_from_our_planet.get(word, (0,0))[0] + 1, 0]
             current_category_dict[word] = [current_category_dict.get(word, (0,0))[0] + 1, 0]
             lengths[row[1]] += 1
             total_words_count += 1
+           
 
-        # if i == 50:
-        #     break
+        if i == 50:
+            break
 
 # print(total_words_count)
 
@@ -49,7 +58,19 @@ with open('filtered_movies.csv', mode='r', encoding='utf-8') as file:
 # print(lengths)
 # exit(0) 
 
-def bayes(word, categ):
+#[print(categories_count[categ] / total_categories_count) for categ in categories_count]
+
+
+def bayes1(word, categ):
+    prob_categ = categories_count[categ] / total_categories_count
+    prob_word = dict_all_words_from_our_planet[word][0]/total_words_count
+    prob_word_cond_categ = categories[categ][word][0]/lengths[categ]
+    if prob_word == 0:
+        return 0
+    result = prob_categ * prob_word_cond_categ / prob_word
+    return round(result,2)
+
+def bayes2(word, categ):
     prob_categ = categories_count[categ] / sum(categories_count.values())
     prob_word = dict_all_words_from_our_planet.get(word, [0, 0])[0] / total_words_count
     prob_word_cond_categ = categories[categ].get(word, [0, 0])[0] / lengths[categ]
@@ -59,21 +80,31 @@ def bayes(word, categ):
     result = prob_categ * prob_word_cond_categ / prob_word
     return round(result, 5)
 
+def remove_words(dict_for_categ):
+    filtered_dict = {key: value for key, value in dict_for_categ.items() if value[0] > 20}
+    return filtered_dict
+
+
+
 
 for key, dict_for_categ in categories.items():
 
     for word, ct in dict_for_categ.items():
-        probability = bayes(word, key)
+        probability = bayes1(word, key)
         dict_for_categ[word][1] = probability
 
+    
+    dict_for_categ = remove_words(dict_for_categ)
+    print(dict_for_categ)
 
-    # filtered_dict = {key: value for key, value in values.items() if value > 20}
+    # filtered_dict = {key: value for key, value in dict_for_categ.items() if value > 20}
     # print(filtered_dict)
 
 
     top_50_items = dict(sorted(dict_for_categ.items(), key=lambda item: item[1][1], reverse=True)[:50])
-    print(len(top_50_items))
-    print(top_50_items)
+    # print(len(top_50_items))
+    # print(top_50_items)
     
-    #print(categories[key])    
+    categories[key] = top_50_items    
 
+#print(categories["horror"])
