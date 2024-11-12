955afec23cd3647496712e3fcba39182844f8001
izabela <izabelajilavu@gmail.com>
2024-11-12 13:35:19 +0200

    more stop words

--- stop_words.py
+++ stop_words.py
@@ -12,5 +12,5 @@ stop_words = {
     "under", "until", "up", "very", "was", "wasn't", "we", "we'd", "we'll", "we're", "we've", "were", "weren't",
     "what", "what's", "when", "when's", "where", "where's", "which", "while", "who", "who's", "whom", "why", "why's",
     "with", "won't", "would", "wouldn't", "you", "you'd", "you'll", "you're", "you've", "your", "yours", "yourself",
-    "yourselves"
+    "yourselves", "s", "one", "new", "two", "three", "get", "year", "will", "family", "must", "can"
 }
